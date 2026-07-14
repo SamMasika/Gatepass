8cb84ed6fbe7cdf961fe9ccdbc6b5f38287866ec
SamMasika <masikasamwel6@gmail.com>
2026-07-14 12:58:10 +0300

    config

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
-  transpileDependencies: ['vuetify'], //  must be an array, not true
-
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? './' : './',
-
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-    },
+    }
   },
-
-  configureWebpack: {
-    resolve: {
-      fallback: {
-        https: require.resolve('https-browserify'),
-        stream: require.resolve('stream-browserify'),
-        process: require.resolve('process/browser'),
-      },
-    },
-  },
-
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-  }
+  // devServer: {
+  //   overlay: false, // Disable the error overlay
+  // },
 })
